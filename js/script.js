bbee0ae837cb806780d12e39e2850159a21204b7
tiegrun <tiko3020@gmail.com>
2020-09-10 04:02:51 -0400

    jQuery
    converted jQuery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var pokemonRepository = (function(){
+var pokemonRepository =(function($) {
   var pokemonList = [];
   var apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
-
+  
   function getAll(){
     return pokemonList;
   }
@@ -11,32 +11,31 @@ var pokemonRepository = (function(){
   }
 
   function addListItem(pokemon){
-    var unorderedList = document.querySelector(".pokemonsList");
-    var listItem = document.createElement("li");
-    var button = document.createElement("button");
-    button.innerText = pokemon.name;
-    button.classList.add("btn");
-    listItem.appendChild(button);
-    unorderedList.appendChild(listItem);
-    button.addEventListener("click", function(){
+    var $unorderedList = $(".pokemonsList");
+    var $listItem = $("<li></li>");
+    var $button = $("<button class='btn'></button>");
+    $button.text(pokemon.name);
+    $listItem.append($button);
+    $unorderedList.append($listItem);
+    $button.on("click", function(){
       showDetails(pokemon);
     });
   }
 
   function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
+    return $.ajax(apiUrl, {
+      dataType: 'json',
     }).then(function (json) {
-      json.results.forEach(function (item) {
-        var pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
-      });
-    }).catch(function (e) {
-      console.error(e);
-    })
+        json.results.forEach(function (item) {
+          var pokemon = {
+            name: item.name,
+            detailsUrl: item,
+          };
+          add(pokemon);
+        });
+      }).catch(function (e) {
+        console.error(e)
+      })
   }
 
   function showDetails(pokemon) {
@@ -46,87 +45,82 @@ var pokemonRepository = (function(){
     });
   }
 
-  function loadDetails(item) {
-    var url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.weight = details.weight;
-      item.type = details.types[0].type.name;
-    }).catch(function (e) {
-      console.error(e);
-    });
+  function loadDetails(pokemon) {
+    var url = pokemon.detailsUrl;
+    return $.ajax(url, { dataType: 'json' }).then(function (details) {
+      pokemon.imageUrl = details.sprites.front_default;
+      pokemon.height = details.height;
+      pokemon.weight = details.weight;
+      pokemon.type = details.types[0].type.name;
+  }).catch(function (e) {
+    console.error(e);
+  });
   }
 
   function showModal(pokemon){
-    var modalContainer = document.querySelector("#modalContainer");
-    var closeModalButton = document.createElement("button");
-    var CloseNode = document.createTextNode("CLOSE");
-    var unorderedList = document.querySelector(".pokemonsList");
-    var profile = document.createElement("div");
-    var profileImage = pokemon.imageUrl;
-    var profileName = pokemon.name;
-    var profileHeight = pokemon.height;
-    var profileWeight = pokemon.weight;
-    var profileType = pokemon.type;
-    modalContainer.classList.remove("modalContainer__isHidden");
-    modalContainer.classList.add("modalContainer__isVisible");
-    unorderedList.style.filter = "blur(5px)";
-    closeModalButton.appendChild(CloseNode);
-    modalContainer.appendChild(profile);
-    modalContainer.appendChild(closeModalButton);
-    profile.innerHTML = `<img src = ${profileImage} alt = ${profileName}> 
-                          <div><h1>name: <span>${profileName}</span></h1>
-                          <h2>height: <span>${profileHeight} m.</span></h2>
-                          <h3>weight: <span>${profileWeight} Kg.</span></h3>
-                          <h4>type: <span>${profileType}</span></h4></div>`;
-    closeModalButton.addEventListener("click", hideModal);
+    var $modalContainer = $("#modalContainer");
+    var $closeModalButton = $("<button>CLOSE</button>");
+    var $unorderedList = $(".pokemonsList");
+    var $profileImage = pokemon.imageUrl;
+    var $profileName = pokemon.name;
+    var $profileHeight = pokemon.height;
+    var $profileWeight = pokemon.weight;
+    var $profileType = pokemon.type;
+    $modalContainer.removeClass("modalContainer__isHidden");
+    $modalContainer.addClass("modalContainer__isVisible");
+    $unorderedList.css("filter", "blur(5px)");
+    var $profile = $('<div><img src="' + $profileImage + '" alt="' + 
+                          $profileName +'"><div><h1>name: <span>'+ 
+                          $profileName +'</span></h1><h2>height: <span>'+ $profileHeight +' M.</span></h2><h3>weight: <span>'+ $profileWeight +' KG.</span></h3><h4>type: <span>'+ $profileType +'</span></h4></div>');
+    
+    $modalContainer.append($profile);
+    $modalContainer.append($closeModalButton);                 
+    $closeModalButton.click(hideModal);                                 
   }
 
-  function hideModal(e){
-    e.target.parentElement.classList.remove("modalContainer__isVisible");
-    e.target.parentElement.classList.add("modalContainer__isHidden");
-    var unorderedList = document.querySelector(".pokemonsList");
-    var modalContainer = document.querySelector("#modalContainer");
-    modalContainer.innerHTML = "";
-    unorderedList.style.filter = "none";
+  function hideModal(event){
+    $(event.target).parent().removeClass("modalContainer__isVisible");
+    $(event.target).parent().addClass("modalContainer__isHidden");
+    var $unorderedList = $(".pokemonsList");
+    var $modalContainer = $("#modalContainer");
+    $modalContainer.html("");
+    $unorderedList.css("style", "none");
   }
 
-  window.addEventListener("keydown", function(e){
-    var modalContainer = document.querySelector("#modalContainer");
-    var unorderedList = document.querySelector(".pokemonsList");
-    if(e.keyCode === 27 && modalContainer.classList.contains("modalContainer__isVisible")){
-      modalContainer.classList.remove("modalContainer__isVisible");
-      modalContainer.classList.add("modalContainer__isHidden");
-      modalContainer.innerHTML = "";
+  $(window).on("keydown", function(event){
+    var $modalContainer = $("#modalContainer");
+    var $unorderedList = $(".pokemonsList");
+    if(event.keyCode === 27 && $modalContainer.hasClass("modalContainer__isVisible")){
+      $modalContainer.removeClass("modalContainer__isVisible");
+      $modalContainer.addClass("modalContainer__isHidden");
+      $modalContainer.html("");
     } 
-    unorderedList.style.filter = "none";
+    $unorderedList.css("filter", "none");
+    // unorderedList.style.filter = "none";
   })
 
-  window.addEventListener("click", function(e){
-    var modalContainer = document.querySelector("#modalContainer");
-    var unorderedList = document.querySelector(".pokemonsList");
-    if(!e.target.classList.contains("modalContainer__isVisible")){
-      modalContainer.classList.remove("modalContainer__isVisible");
-      modalContainer.classList.add("modalContainer__isHidden");
-      unorderedList.style.filter = "none";
-      modalContainer.innerHTML = "";
+  $(window).click(function(event){
+    var $modalContainer = $("#modalContainer");
+    var $unorderedList = $(".pokemonsList");
+    if(!$(event.target).hasClass("modalContainer__isVisible")){
+      $modalContainer.removeClass("modalContainer__isVisible");
+      $modalContainer.addClass("modalContainer__isHidden");
+      $unorderedList.css("filter", "none");
+      $modalContainer.html("");
     }
   })
 
   return {
-    getAll: getAll,
-    add: add,
-    addListItem: addListItem,
-    loadList: loadList,
-    loadDetails: loadDetails,
-    showDetails: showDetails,
-    showModal: showModal,
-    hideModal: hideModal
+    getAll,
+    add,
+    addListItem,
+    loadList,
+    loadDetails,
+    showDetails,
+    showModal,
+    hideModal
   };
-})();
+})(jQuery);
 
 pokemonRepository.loadList().then(function() {
   pokemonRepository.getAll().forEach(function(pokemon){
